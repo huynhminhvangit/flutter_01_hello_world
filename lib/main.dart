c9aaaf5c16261d33d523184517ed3ef9ac07aa8f
Vang Huynh <vanghm@imt-soft.com>
2021-07-23 21:14:24 +0700

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -8,35 +8,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello, World!',
+      title: 'Bai 4',
       theme: ThemeData(
-        primaryColor: Colors.blue,
+        primaryColor: Colors.blue.shade100,
       ),
-      home: HomePage(),
+      home: Home(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello, World!'),
+        title: Text('Bai 4'),
         centerTitle: true,
       ),
-      body: Container(
-        color: Colors.pink,
-        child: Align(
-          alignment: Alignment.center,
-          child: Text('Hello, World!!!'),
+      body: Center(
+        child: Text('Hello, world!!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Click',
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.amber.shade300,
+          primaryColor: Colors.blue.shade100,
+          textTheme: Theme.of(context).textTheme.copyWith(
+                caption: TextStyle(
+                  color: Colors.white70,
+                ),
+              ),
+        ),
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+            BottomNavigationBarItem(icon: Icon(Icons.remove), label: 'Remove'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.my_location), label: 'Location'),
+          ],
+          currentIndex: 3,
+          fixedColor: Colors.blue,
         ),
       ),
+      drawer: Drawer(
+
+      ),
+      endDrawer: Drawer(),
     );
   }
 }
