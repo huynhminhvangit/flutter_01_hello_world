9b68e450d02f70b05fc1a8743de4cc29a720fea2
Vang Huynh <vanghm@imt-soft.com>
2021-07-24 09:59:15 +0700

    bai5 container

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,33 +11,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello, World!',
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: HomePage(),
+      title: 'Bai 5',
+      theme: ThemeData(primaryColor: Colors.blue.shade100),
+      home: Home(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello, World!'),
+        title: Text('Bai 5'),
         centerTitle: true,
       ),
-      body: Container(
-        color: Colors.pink,
-        child: Align(
+      body: Center(
+        child: Container(
           alignment: Alignment.center,
-          child: Text('Hello, World!!!'),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border.all(width: 5, color: Colors.red,style: BorderStyle.solid),
+            shape: BoxShape.circle,
+            color: Colors.blue
+          ),
+          transform: Matrix4.rotationZ(30 * pi / 180),
+          width: 200,
+          height: 200,
+          child: Text('Bai 5',style: TextStyle(color: Colors.white70, fontSize: 20),),
         ),
       ),
     );
